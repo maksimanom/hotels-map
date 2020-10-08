5ba9a31371136e5d9a6051bd15d18d53202089e4
maksimanom <maksymKozoriz@gmail.com>
2020-10-08 11:17:35 +0300

    first commit

---
+++ src/constants/API-example.ts
@@ -0,0 +1,6 @@
+export const BASE_URL = "https://places.ls.hereapi.com";
+export const PATH = "/places/v1/";
+export const DISCOVER_SEARCH = "discover/search";
+export const DISCOVER_EXPLORE = "discover/explore";
+export const APP_ID = "";
+export const API_KEY = "";
